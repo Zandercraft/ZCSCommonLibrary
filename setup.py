b479e1c7ae580eb200a4ed40d07ee1dc94a167f4
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-02-16 17:48:32 -0500

    Version bump and final preparations for PyPi

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='zcscommonlib',
     packages=find_packages(include=['zcscommonlib']),
-    version='0.1.0',
+    version='0.2.0',
     description='A Common Library For Use In Computer Science Projects',
     author='Zandercraft',
     url='https://github.com/Zandercraft/ZCSCommonLibrary',
+    download_url='https://github.com/Zandercraft/ZCSCommonLibrary/archive/0.2.0.tar.gz',
     license='MPL 2.0',
     install_requires=[],
     setup_requires=['pytest-runner'],
